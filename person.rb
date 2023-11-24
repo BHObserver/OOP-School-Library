7d7b663ffde23473c9c03d310600b59ad93bbd2f
BHObserver <BHObserver0@gmail.com>
2023-11-24 14:46:45 +0600

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,3 +1,12 @@
 class Person
+  attr_reader :id, :name, :age
+
+  def initialize(id, age = 0, parent_permission: true, name: "Unknown")
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
 
 end
