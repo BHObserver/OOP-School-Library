fc8ef751e1dd350831c6c7b9bdbb9f9dd45b7fe3
BHObserver <BHObserver0@gmail.com>
2023-11-24 15:08:41 +0600

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,16 +1,27 @@
-class Person
-  attr_accessor :name, :age
-  attr_reader :id
+class Person < Nameable
+  attr_reader :id, :name, :age
 
-  def initialize(id, age = 0, parent_permission: true, name: 'Unknown')
+  def initialize(id, age = 0, parent_permission: true, name: "Unknown")
     @id = id
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def age=(new_age)
+    @age = new_age
+  end
+
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
