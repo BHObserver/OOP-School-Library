78edd8a09d0c2f3bcd991796cd98591e17080482
BHObserver <BHObserver0@gmail.com>
2023-11-24 15:11:13 +0600

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,7 +1,8 @@
 class Person < Nameable
   attr_reader :id, :name, :age
 
-  def initialize(id, age = 0, parent_permission: true, name: "Unknown")
+  def initialize(id, age = 0, parent_permission: true, name: 'Unknown')
+    super()
     @id = id
     @name = name
     @age = age
