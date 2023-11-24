a4d7b40914b2fadfaef1f3ba306c91ee7fa0babd
BHObserver <BHObserver0@gmail.com>
2023-11-24 21:24:29 +0600

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,28 +1,13 @@
-require_relative 'nameable'
+class Person
+  attr_accessor :name
+  attr_reader :rentals
 
-class Person < Nameable
-  attr_accessor :name, :age
-  attr_reader :id
-
-  def initialize(id, age = 0, parent_permission: true, name: 'Unknown')
-    super()
-    @id = id
+  def initialize(name)
     @name = name
-    @age = age
-    @parent_permission = parent_permission
+    @rentals = []
   end
 
-  def can_use_services?
-    of_age? || @parent_permission
-  end
-
-  def correct_name
-    @name
-  end
-
-  private
-
-  def of_age?
-    @age >= 18
+  def add_rental(rental)
+    @rentals << rental
   end
 end
