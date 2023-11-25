0f7f8d0387d1b8f6e3af1a825f6ecf6d91d301a1
BHObserver <BHObserver0@gmail.com>
2023-11-25 23:11:37 +0600

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,13 +1,11 @@
-class Student
-  attr_accessor :name, :classroom
+require './person'
 
-  def initialize(name)
-    @name = name
-    @classroom = nil
-  end
+# Student class a child of a Person class
+class Student < Person
+  attr_reader :classroom
 
-  def belongs_to_classroom(classroom)
+  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
-    classroom&.add_student(self)
   end
 end
