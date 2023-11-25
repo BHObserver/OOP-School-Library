c08d4512a3eee181ff30533d9417bf99b784345c
BHObserver <BHObserver0@gmail.com>
2023-11-25 23:11:41 +0600

    Update student.rb

--- student.rb
+++ student.rb
@@ -8,4 +8,13 @@ class Student < Person
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
 end
