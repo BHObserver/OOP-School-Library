bbe7fb1b6dbb673ea81ac64b6f1a5c118ea2db1a
BHObserver <BHObserver0@gmail.com>
2023-11-25 23:10:24 +0600

    Update person.rb

--- person.rb
+++ person.rb
@@ -22,4 +22,14 @@ class Person < Nameable
   def correct_name
     @name
   end
+
+  def add_rental(date, book)
+    Rental.new(date, book, self)
+  end
+
+  private
+
+  def of_age?
+    true if @age >= 18
+  end
 end
