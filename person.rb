6d9f9b40c05ba089d119adb4f0163825731cdbc7
BHObserver <BHObserver0@gmail.com>
2023-11-25 08:57:02 +0600

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,10 +1,15 @@
 class Person
   attr_accessor :name
-  attr_reader :rentals
+  attr_reader :students, :rentals
 
   def initialize(name)
     @name = name
+    @students = []
     @rentals = []
+  end
+
+  def add_student(student)
+    @students << student
   end
 
   def add_rental(rental)
