4b6185215cc02d8f12f0a10b135b603f658b8442
BHObserver <BHObserver0@gmail.com>
2023-11-25 08:56:59 +0600

    Update main.rb

--- main.rb
+++ main.rb
@@ -23,6 +23,25 @@ student.classroom = classroom
 book = Book.new('Ruby Basics', 'John Smith')
 Rental.new('2023-11-24', person, book)
 
+person = Person.new('Teacher Bob')
+
+student1 = Student.new('John Doe')
+student2 = Student.new('Jane Smith')
+
+person.add_student(student1)
+person.add_student(student2)
+
+classroom = Classroom.new('Math Class')
+
+student1.belongs_to_classroom(classroom)
+student2.belongs_to_classroom(classroom)
+
+puts "Person: #{person.name}"
+puts "Students for Person: #{person.students.map(&:name)}"
+puts "Classroom for Student1: #{student1.classroom&.label}"
+puts "Classroom for Student2: #{student2.classroom&.label}"
+
+
 # Output information
 puts "Classroom: #{classroom.label}"
 puts "Students in Classroom: #{classroom.students.map(&:name)}"
