409d75fcd1b0192a7f04e1f2de9d262229952687
BHObserver <BHObserver0@gmail.com>
2023-11-25 21:47:43 +0600

    Update app.rb

--- app.rb
+++ app.rb
@@ -12,4 +12,15 @@ class App
     @people = []
     @rentals = []
   end
+
+  def book_list(show_index: false)
+    if @books.empty?
+      puts 'Sorry, the library is empty. Please add some books.'
+    else
+      @books.each_with_index do |book, index|
+        puts "#{show_index ? index : ' '} Title: \"#{book.title}\", Author: \"#{book.author}\"."
+      end
+    end
+    puts
+  end
 end
