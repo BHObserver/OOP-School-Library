d76291ec80ac5576d0cd4a3f44a15b53243ca2a3
BHObserver <BHObserver0@gmail.com>
2023-11-25 23:14:31 +0600

    Update rental.rb

--- rental.rb
+++ rental.rb
@@ -1,13 +1,4 @@
+# Rental class
 class Rental
-  attr_accessor :date
-  attr_reader :person, :book
-
-  def initialize(date, person, book)
-    @date = date
-    @person = person
-    @book = book
-
-    person.add_rental(self)
-    book.add_rental(self)
-  end
+  attr_accessor :date, :book, :person
 end
