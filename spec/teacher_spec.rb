62193cdd52b50e3e534af03b92d007c6ddc3d5ca
BHObserver <BHObserver0@gmail.com>
2023-12-02 22:28:28 +0600

    Update teacher_spec.rb

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -7,4 +7,12 @@ describe Teacher do
     expect(teacher.age).to eq(0)
     expect(teacher.name).to eq('John Doe')
   end
+
+  # Test the can_use_services? method
+  describe '#can_use_services?' do
+    it 'returns true' do
+      teacher = Teacher.new(1, 'Math')
+      expect(teacher.can_use_services?).to be(true)
+    end
+  end
 end
