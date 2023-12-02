7e7ebdb6d5187c15042ba57c043dbacc6b6ad37e
BHObserver <BHObserver0@gmail.com>
2023-12-02 23:27:21 +0600

    update files

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -28,3 +28,48 @@ RSpec.describe Person do
     end
   end
 end
+
+# person_add_rental_spec.rb
+require_relative '../person'
+require_relative '../rental'
+require_relative '../book' # Make sure to require any necessary dependencies
+
+RSpec.describe Person do
+  let(:person_age) { 25 }
+  let(:person_name) { 'John Doe' }
+  let(:parent_permission) { true }
+  # Adjust with the appropriate Book class and initialization
+  let(:book) do
+    Book.new('The Great Gatsby', 'F. Scott Fitzgerald')
+  end
+  subject(:person) { described_class.new(person_age, person_name, parent_permission: parent_permission) }
+
+  describe '#add_rental' do
+    it 'creates a new rental' do
+      rental_date = '2023-01-01'
+      rental = person.add_rental(rental_date, book)
+
+      expect(rental).to be_an_instance_of(Rental)
+      expect(rental.date).to eq(rental_date)
+      expect(rental.book).to eq(book)
+      expect(rental.person).to eq(person)
+    end
+  end
+end
+
+# person_correct_name_spec.rb
+require_relative '../person'
+
+RSpec.describe Person do
+  let(:person_age) { 25 }
+  let(:person_name) { 'John Doe' }
+  let(:parent_permission) { true }
+
+  subject(:person) { described_class.new(person_age, person_name, parent_permission: parent_permission) }
+
+  describe '#correct_name' do
+    it 'returns the correct name' do
+      expect(person.correct_name).to eq(person_name)
+    end
+  end
+end
