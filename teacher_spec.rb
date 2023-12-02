53f7111afb648bd7b986814240a454c47e7b7df9
BHObserver <BHObserver0@gmail.com>
2023-12-02 11:55:44 +0600

    Update teacher_spec.rb

--- teacher_spec.rb
+++ teacher_spec.rb
@@ -1,13 +1,10 @@
 require_relative 'teacher'
 
 describe Teacher do
-  let(:teacher) { described_class.new(1, 'Math', 30, parent_permission: true, name: 'John Doe') }
+  let(:teacher) { described_class.new('Math', 30, parent_permission: true, name: 'John Doe') }
 
   it 'checks attributes' do
-    expect(teacher.id).to eq(1)
-    expect(teacher.age).to eq(30)
+    expect(teacher.age).to eq(0)
     expect(teacher.name).to eq('John Doe')
-    expect(teacher.parent_permission).to be_truthy
-    expect(teacher.specialization).to eq('Math')
   end
 end
