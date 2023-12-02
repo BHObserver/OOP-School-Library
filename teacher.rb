6003627668fc800a806bb297cb51c57c0d686ec6
BHObserver <BHObserver0@gmail.com>
2023-12-02 11:55:41 +0600

    Update teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -1,9 +1,10 @@
-require_relative 'person'
+require './person'
 
 class Teacher < Person
-  def initialize(id, specialization, age = 0, name: 'Unknown', parent_permission: true)
-    super(id, specialization, age, name, parent_permission: parent_permission)
+  def initialize(id, specialization, _age = 0, name: 'Unknown', parent_permission: true)
+    super(id, name, parent_permission: parent_permission)
     @specialization = specialization
+    @age = 0
   end
 
   def can_use_services?
